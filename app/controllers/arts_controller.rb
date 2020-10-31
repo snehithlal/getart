3ad85d6a51b08076946345b52a2ac737a2bec03c
snehithlal <snehithlal@foradian.com>
2020-10-31 09:09:37 +0530

    refactor: added include

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -2,7 +2,7 @@ class ArtsController < ApplicationController
   before_action :login_required, except: [:index]
 
   def index
-    @categories = Category.all
+    @categories = Category.all.includes(:subcategories)
   end
   
 end
